78d48a2f7caeea8f70b89174fff221278994d005
Jacob Marks <jacobjmarks@outlook.com>
2017-10-18 17:02:28 +1000

    Scripting updates.

--- run-parallel.sh
+++ run-parallel.sh
@@ -1,3 +1,4 @@
 rm -f parallel.out
 g++ -O2 -fopenmp cvtree-parallel.cpp -o parallel.out
 ./parallel.out data/
+rm -f parallel.out
--- run-seqential.sh
+++ run-seqential.sh
@@ -1,3 +1,4 @@
 rm -f sequential.out
 g++ -O2 cvtree-sequential.cpp -o sequential.out
 ./sequential.out data/
+rm -f sequential.out
